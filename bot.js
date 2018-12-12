fa1e6a697f94df5bbc3fa9282c39a76213149b75
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-12 00:19:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,51 @@
 const Discord = require('discord.js');
-const prefix = 'T'
-const client = new Discord.Client({ disableEveryone: true});
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-const ytdl = require('ytdl-core');
+
+const Util = require('discord.js');
+
 const getYoutubeID = require('get-youtube-id');
+
 const fetchVideoInfo = require('youtube-info');
+
 const YouTube = require('simple-youtube-api');
+
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 
-let active = new Map();
+const queue = new Map();
 
-client.on('warn', console.warn);
+const ytdl = require('ytdl-core');
 
-client.on('error', console.error);
+const fs = require('fs');
 
-client.on('ready', () => {
-    console.log(`Created By: Mahmoud-QuaStyle`);
-    console.log(`Developed By: ! Mahmoud-QuaStyle ♥`);
-    console.log(`Guilds: ${client.guilds.size}`);
-    console.log(`Users: ${client.users.size}`);
-    client.user.setActivity('Alhassny Orders.',{type: 'WATCHING'});
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
+const prefix = "";
+/////////////////////////
+////////////////////////
+
+client.on('message', async msg =>{
+	if (msg.author.bot) return undefined;
+    if (!msg.content.startsWith(prefix)) return undefined;
+    
+    let args = msg.content.split(' ');
+
+	let command = msg.content.toLowerCase().split(" ")[0];
+	command = command.slice(prefix.length)
+
+    if(command === `ping`) {
+    let embed = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTitle("Pong!!")
+    .setDescription(`${client.ping} ms,`)
+    .setFooter(`Requested by | ${msg.author.tag}`);
+    msg.delete().catch(O_o=>{})
+    msg.channel.send(embed);
+    }
 });
+
+
+
 
 client.on('message', async msg => {
     if(msg.author.bot) return undefined;
